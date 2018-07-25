1e4e8e71853c3754ca37ac5c668971ba5e08412e
johnqu1982@gmail.com <johnqu1982@gmail.com>
2018-07-25 18:43:44 +0800

    get certificate of 6002

---
+++ 6002xExercises/Quiz/solviet.py
@@ -0,0 +1,20 @@
+def solveit(test):
+    """ test, a function that takes an int parameter and returns a Boolean
+        Assumes there exists an int, x, such that test(x) is True
+        Returns an int, x, with the smallest absolute value such that test(x) is True
+        In case of ties, return any one of them.
+    """
+    x = 0
+    while not test(x):
+        x += 1
+    return x
+
+#### This test case prints 49 ####
+def f(x):
+    return (x+15)**0.5 + x**0.5 == 15
+print(solveit(f))
+
+#### This test case prints 0 ####
+def f(x):
+    return x == 0
+print(solveit(f))
